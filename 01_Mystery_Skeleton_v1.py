80e69da52bcc1623688c34714c7ec5cc89f2f076
zhenh65671 <61208218+zhenh65671@users.noreply.github.com>
2020-05-29 13:00:29 +1200

    1

--- 01_Mystery_Skeleton_v1.py
+++ 01_Mystery_Skeleton_v1.py
@@ -14,11 +14,20 @@ class Start:
                                        font="Arial 19 bold")
         self.mystery_box_label.grid(row=1)
 
-        # Entry box... (row 1)
+        # Initial Instructions (row 1)
+        self.mystery_instructions = Label(self.start_frame, fon="Arial 10 italic",
+                                          text="Please enter a dollar amount"
+                                               "(between $5 and $50) in the box"
+                                               "below. Then choose the stakes."
+                                               "The higher the stakes, the more you can win!",
+                                          wrap=288, justidy=LEFT, padx=10, pady=10)
+        self.mystery_instructions.grid(row=1)
+
+        # Entry box... (row 2)
         self.start_amount_entry = Entry(self.start_frame, font="Arial 16 bold")
         self.start_amount_entry.grid(row=2)
 
-        # Play Button (row 2)
+        # Play Button (row 3)
         self.lowstakes_button = Button(text="Low ($5)",
                                        command=lambda: self.to_game(1))
         self.lowstakes_button.grid(row=2, padx=10)
